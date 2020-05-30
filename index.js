0a733b020cab5739ac700a2819c0a4c9c6829ed9
Chang Kiat <>
2020-05-30 21:31:53 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8000;
+var cors = require("cors");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.use(function (err, req, res, next) {
+  console.log(err.stack);
+  res.status(500).send("An error has occurred");
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send("Unable to find API");
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
