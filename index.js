74b9d763cddc29cb7ca4ea6e3bd652dffae3cf7b
Chang Kiat <>
2020-05-31 13:20:03 +0800

    Fix IP address not printing. Remove tag value from container image URL in deployment.yaml

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-var serverIP;
-
 app.get("/", function (req, res) {
-  res.send("Running at http://" + serverIP + ":" + port + "<p/> v1");
+  var address,
+    ifaces = require("os").networkInterfaces();
+  for (var dev in ifaces) {
+    ifaces[dev].filter((details) =>
+      details.family === "IPv4" && details.internal === false
+        ? (address = details.address)
+        : undefined
+    );
+  }
+  console.log();
+  res.send(`IP Address: ${address} <p/>v1`);
 });
 
 app.use(function (err, req, res, next) {
@@ -22,7 +30,6 @@ app.use(function (req, res, next) {
   res.status(404).send("Unable to find API");
 });
 
-var server = app.listen(port, function () {
-  serverIP = server.address().address;
-  console.log("Running at http://" + serverIP + ":" + port);
+app.listen(port, function () {
+  console.log("Listening at port " + port);
 });
