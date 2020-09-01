b067da7595fe22a1d5e798548f6968979fac99e4
Tan Chang Kiat <tanchangkiat@gmail.com>
2020-09-01 14:34:53 +0800

    Added gzip compression

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var compression = require("compression");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
@@ -7,6 +8,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(compression());
 
 app.get("/", async function (req, res) {
   const cpu = await si.cpu();
