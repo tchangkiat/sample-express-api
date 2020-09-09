f43aeb1451e31bcb77a49d1162e6bc668b7c1203
Tan Chang Kiat <tanchangkiat@gmail.com>
2020-09-09 16:52:12 +0800

    Added helmet package

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var compression = require("compression");
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 8000;
+const helmet = require("helmet");
 const si = require("systeminformation");
 const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(helmet());
 app.use(compression());
 
 app.get("/", async function (req, res) {
