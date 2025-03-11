a6f0b1bb29cff2e2ce75042287fb2a4af18a3eef
Tan Chang Kiat <tanchangkiat@gmail.com>
2025-03-11 07:52:16 +0000

    Minor amendments to pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,6 @@ module.exports = {
     name: "sample-express-api",
     script: "./index.js",
     exec_mode: "cluster",
-    instances: "3",
-    env: {
-      NODE_ENV: "development",
-    },
-    env_production: {
-      NODE_ENV: "production",
-    }
+    instances: "3"
   }]
 }
